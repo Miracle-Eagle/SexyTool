52ff0f7af3b2d6950f02f4ee038aa6f6839950b1
Miracle-Eagle <111931709+Miracle-Eagle@users.noreply.github.com>
2023-10-18 11:22:03 -0400

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[ assembly : AssemblyTitle("Sexy Tool") ]
+[ assembly : AssemblyDescription("With this Tool, you'll be Able to Manage the Files from PopCap Games.") ]
+[ assembly : AssemblyCompany("Miracle Softwares") ]
+[ assembly : AssemblyProduct("Sexy Tool: the PopCap Fans App") ]
+
+# if DEBUG
+[ assembly : AssemblyConfiguration("Debug") ]
+
+# else
+[ assembly : AssemblyConfiguration("Release") ]
+
+# endif
+
+[ assembly : ComVisible(false) ]
+[ assembly : Guid("7a0ac07f-c08b-4e44-ad91-3f0ef1621a72") ]
+[ assembly : AssemblyVersion("1.0.*") ]
+[ assembly : AssemblyFileVersion("1.0.0.0") ]
